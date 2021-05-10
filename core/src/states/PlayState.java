9c530a38bb79ba92e3fac5c87e63d67227f20099
Arseniy <ars.gusev1304@yandex.ru>
2021-05-10 22:05:28 +0300

    Last

--- core/src/states/PlayState.java
+++ core/src/states/PlayState.java
@@ -1,31 +1,39 @@
 package states;
 
+import com.ars.game.MyGame;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import sprites.Bird;
+
 public class PlayState extends State {
 
-    private Texture bird;
+    private Bird bird;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 300);
+        camera.setToOrtho(false, MyGame.width / 2, MyGame.height / 2);
     }
 
     @Override
     protected void handleInput() {
-
+        if (Gdx.input.justTouched())
+            bird.jump();
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
+        bird.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(camera.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
 
